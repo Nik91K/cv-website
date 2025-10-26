874a7ab60de1e34b81a79955174e95e3c7e08cbd
Nik91K <nikotakovsl@gmail.com>
2025-10-27 00:18:50 +0200

    updaet types, add MainPage type

--- src/types/Header.ts
+++ src/types/Header.ts
@@ -1,4 +1,4 @@
-export type NavigationItem = {
+type NavigationItem = {
     id: string,
     label: string,
     path: string
---
+++ src/types/MainPage.ts
@@ -0,0 +1,36 @@
+type HeroSection = {
+    title: string,
+    subtitle: string,
+    image: string,
+    imageTitle: string,
+}
+
+export type HeroSectionProps = {
+    items?: HeroSection[],
+}
+
+type Experience = {
+  company: string,
+  role: string,
+  years: string,
+}
+
+type Contacts = {
+    email: string,
+    github?: string,
+    linkedin?: string,
+    twitter?: string,
+    website?: string,
+}
+
+type Skills = {
+    title?: string,
+    skills?: string[]
+}
+
+export type AboutItemProps = {
+    id?: string,
+    title?: string,
+    text?: string,
+    skills?: Skills[]
+}
