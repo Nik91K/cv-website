56db76b2ad00eee0e23ce400cfb174b7f614d450
Nik91K <nikotakovsl@gmail.com>
2026-05-27 20:43:17 +0200

    fix a bug with scrolling

--- src/layoutPage/index.tsx
+++ src/layoutPage/index.tsx
@@ -3,10 +3,12 @@ import React from 'react';
 import ScrollToTopButton from '@components/common/Buttons/ScrollToTop';
 import ResponsiveHeader from '@components/layout/Header/ResponsiveHeader';
 import { Analytics } from '@vercel/analytics/react';
+import { ScrollRestoration } from 'react-router-dom';
 
 const LayoutPage = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="bg-[#16161a] flex min-h-screen flex-col">
+      <ScrollRestoration />
       <Analytics />
       <ResponsiveHeader />
       <main className="w-full flex-1">
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import '@/index.css';
 import MainPage from '@/pages/Home';
 import NotFoundPage from '@/pages/NotFound';
 import ProjectPage from '@/pages/Project';
 
+const router = createBrowserRouter([
+  { path: '/', element: <MainPage /> },
+  { path: '/projects/:id', element: <ProjectPage /> },
+  { path: '*', element: <NotFoundPage /> },
+]);
+
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/projects/:id" element={<ProjectPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
-  </BrowserRouter>
+  <RouterProvider router={router} />
 );
